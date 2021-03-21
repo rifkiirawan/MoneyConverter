03c10c8f30f670657769c0e3c943afafbb34b6ab
rifkiirawan <rifkiauliairawan@gmail.com>
2021-03-21 14:21:38 +0700

    First Commit

---
+++ MoneyConverter/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace MoneyConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.Location = new System.Drawing.Point(318, 111);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(247, 35);
+            this.textBox3.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "IDR",
+            "USD",
+            "JPY",
+            "EUR"});
+            this.comboBox1.Location = new System.Drawing.Point(260, 179);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 38);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "IDR",
+            "USD",
+            "JPY",
+            "EUR"});
+            this.comboBox2.Location = new System.Drawing.Point(495, 179);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 38);
+            this.comboBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Berlin Sans FB", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(226, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(366, 54);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Money Converter";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Berlin Sans FB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(231, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 26);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Value :";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Berlin Sans FB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(178, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 26);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "From :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Berlin Sans FB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(442, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 26);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "To :";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Font = new System.Drawing.Font("Lucida Console", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(314, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 54);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(249, 348);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 32);
+            this.label5.TabIndex = 12;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.Control;
+            this.label6.Font = new System.Drawing.Font("Berlin Sans FB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(318, 353);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 26);
+            this.label6.TabIndex = 13;
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox3);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ MoneyConverter/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MoneyConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == "IDR" && comboBox2.SelectedItem == "JPY")
+            {
+                label5.Text = "JPY :";
+
+                if (string.IsNullOrEmpty(textBox3.Text))
+                {
+                    label6.Text = "Insert a number.";
+                    label6.ForeColor = Color.Red;
+                }
+                else
+                {
+                    double val = double.Parse(textBox3.Text);
+                    val = val * 0.0076;
+
+                    label6.Text = val.ToString();
+                    label6.ForeColor = Color.Black;
+                }
+            }
+            else if (comboBox1.SelectedItem == "IDR" && comboBox2.SelectedItem == "USD") //idr-usd
+            {
+                label5.Text = "USD :";
+
+                if (string.IsNullOrEmpty(textBox3.Text))
+                {
+                    label6.Text = "Insert a number.";
+                    label6.ForeColor = Color.Red;
+                }
+                else
+                {
+                    double val = double.Parse(textBox3.Text);
+                    val = val * 0.00007;
+
+                    label6.Text = val.ToString();
+                    label6.ForeColor = Color.Black;
+                }
+            }
+            else if (comboBox1.SelectedItem == "JPY" && comboBox2.SelectedItem == "IDR") //jpy-idr
+            {
+                label5.Text = "IDR :";
+
+                if (string.IsNullOrEmpty(textBox3.Text))
+                {
+                    label6.Text = "Insert a number.";
+                    label6.ForeColor = Color.Red;
+                }
+                else
+                {
+                    double val = double.Parse(textBox3.Text);
+                    val = val * 131.81;
+
+                    label6.Text = val.ToString();
+                    label6.ForeColor = Color.Black;
+                }
+            }
+            else if (comboBox1.SelectedItem == "JPY" && comboBox2.SelectedItem == "USD") //jpy-usd
+            {
+                label5.Text = "USD :";
+
+                if (string.IsNullOrEmpty(textBox3.Text))
+                {
+                    label6.Text = "Insert a number.";
+                    label6.ForeColor = Color.Red;
+                }
+                else
+                {
+                    double val = double.Parse(textBox3.Text);
+                    val = val * 0.0092;
+
+                    label6.Text = val.ToString();
+                    label6.ForeColor = Color.Black;
+                }
+            }
+            else if (comboBox1.SelectedItem == "USD" && comboBox2.SelectedItem == "IDR") //usd-idr
+            {
+                label5.Text = "IDR :";
+
+                if (string.IsNullOrEmpty(textBox3.Text))
+                {
+                    label6.Text = "Insert a number.";
+                    label6.ForeColor = Color.Red;
+                }
+                else
+                {
+                    double val = double.Parse(textBox3.Text);
+                    val = val * 14372.25;
+
+                    label6.Text = val.ToString();
+                    label6.ForeColor = Color.Black;
+                }
+            }
+            else if (comboBox1.SelectedItem == "USD" && comboBox2.SelectedItem == "JPY") //usd-jpy
+            {
+                label5.Text = "JPY :";
+
+                if (string.IsNullOrEmpty(textBox3.Text))
+                {
+                    label6.Text = "Insert a number.";
+                    label6.ForeColor = Color.Red;
+                }
+                else
+                {
+                    double val = double.Parse(textBox3.Text);
+                    val = val * 108.92;
+
+                    label6.Text = val.ToString();
+                    label6.ForeColor = Color.Black;
+                }
+            }
+            else
+            {
+                label5.Text = "";
+                label6.Text = "Choose the currency";
+                label6.ForeColor = Color.Red;
+            }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
